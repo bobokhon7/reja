9ebb8cf90bb4a6c35363d24b8a73e465533cfc1a
Bobokhon <rboboxon6055@gmail.com>
2024-02-14 10:16:11 +0900

    feat: CRUD is done and Task D solved

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 
 // MongoDB chaqirish:
 const db = require("./server").db();
+const mongodb = require("mongodb");
 
 //1 enter code
 app.use(express.static("public"));
@@ -22,13 +23,42 @@ app.post("/create-item", (req, res) => {
   console.log(req.body);
   const new_reja = req.body.reja;
   db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
-    if (err) {
-      console.log(err);
-      res.end("Something went wrong!");
-    } else {
-      res.end(`Successfully added`);
-    }
+    res.json(data.ops[0]);
   });
+});
+
+///// delete
+app.post("/delete-item", (req, res) => {
+  const id = req.body.id;
+  db.collection("plans").deleteOne(
+    { _id: new mongodb.ObjectId(id) },
+    function (err, data) {
+      res.json({ state: "success" });
+    }
+  );
+});
+
+///// edit
+
+app.post("/edit-item", (req, res) => {
+  const data = req.body;
+  db.collection("plans").findOneAndUpdate(
+    { _id: new mongodb.ObjectId(data.id) },
+    { $set: { reja: data.new_input } },
+    function (err, data) {
+      res.json({ state: "success" });
+    }
+  );
+});
+
+/// delete all
+
+app.post("/delete-all", (req, res) => {
+  if (req.body.delete_all) {
+    db.collection("plans").deleteMany(function () {
+      res.json({ state: "all deleted" });
+    });
+  }
 });
 
 app.get("/", function (req, res) {
--- train.js
+++ train.js
@@ -1,21 +1,94 @@
+// /*D-TASK:
+
+// Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin,
+// hamda classning 3ta methodi bolsin, biri qoldiq, biri sotish va biri qabul. Har bir method
+// ishga tushgan vaqt ham log qilinsin.
+// MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon
+// va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non,
+//  5ta lagmon va 6ta cola mavjud!
+
+class Shop {
+  // constructor
+  constructor(non, lagmon, cola) {
+    this.non = non;
+    this.lagmon = lagmon;
+    this.cola = cola;
+  }
+
+  sotish(non) {
+    this.non -= non;
+  }
+
+  qabul(cola) {
+    this.cola += cola;
+  }
+
+  qoldiq() {
+    let now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    console.log(
+      `hozir ${hours}:${minutes} da ${this.non} ta non va ${this.lagmon} ta lagmon va ${this.cola} ta cola mavjud!`
+    );
+  }
+}
+const shop = new Shop(4, 5, 2);
+shop.qoldiq();
+shop.sotish(3);
+shop.qabul(4);
+shop.qoldiq();
+
+// const animal_List = [
+//   "fox",
+//   "ant",
+//   "bird",
+//   "lion",
+//   "wolf",
+//   "deer",
+//   "frog",
+//   "hen",
+//   "mole",
+//   "duck",
+//   "dog",
+//   "cock",
+//   "cow",
+// ];
+
+// const findAnimal = (txt) => {
+//   let newArr = [];
+
+//   for (let i = 0; i < animal_List.length; i++) {
+//     for (let b = 0; b < animal_List[i].length; b++) {
+//       if (animal_List[i][b].includes(txt)) {
+//         newArr.push(animal_List[b]);
+//       }
+//     }
+//   }
+
+//   return newArr;
+// };
+
+// console.log(findAnimal("fdgwocalt"));
+
 // B-TASK:
 
 // Shunday function tuzing, u 1ta string parametrga ega bolsin,
 // hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
 // MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
 
-const countDigits = (data) => {
-  let arr = [];
-  let changeArr = data.split("");
-  for (i = 0; i < changeArr.length; i++) {
-    if (!isNaN(changeArr[i])) {
-      arr.push(changeArr[i]);
-    }
-  }
+// const countDigits = (data) => {
+//   let arr = [];
+//   let changeArr = data.split("");
+//   for (i = 0; i < changeArr.length; i++) {
+//     if (!isNaN(changeArr[i])) {
+//       arr.push(changeArr[i]);
+//     }
+//   }
 
-  return arr.length;
-};
-console.log(countDigits("ad2a54y79wet0sfgb9"));
+//   return arr.length;
+// };
+// console.log(countDigits("ad2a54y79wet0sfgb9"));
 // A-TASK:
 // Shunday 2 parametrli function tuzing,
 // hamda birinchi parametrdagi letterni ikkinchi
